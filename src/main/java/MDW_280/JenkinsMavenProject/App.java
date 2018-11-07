aefaa80537658727b75cb1aa5404cce39110a013
hermannozor <hermannfotso2@gmail.com>
2018-11-07 12:36:45 +0100

    Initial commit

---
+++ src/main/java/MDW_280/JenkinsMavenProject/App.java
@@ -0,0 +1,13 @@
+package MDW_280.JenkinsMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
