2578b2a03952ec4c15c5aa18334fc3b4d447af77
hermannozor <hermannfotso2@gmail.com>
2018-11-07 14:39:51 +0100

    Commit 2
    second commit

--- src/main/java/MDW_280/JenkinsMavenProject/App.java
+++ src/main/java/MDW_280/JenkinsMavenProject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Jenkins is great!");
+        System.out.println( "He )
+        System.out.println("Jenkins is )
     }
 }
